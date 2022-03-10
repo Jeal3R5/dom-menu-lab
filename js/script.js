54a2995686429f0d28ff3618b4420a8ada1526b7
jrenfroe <joan.renfroe@gmail.com>
2022-03-09 19:15:03 -0800

    domMenuLabPush

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let mainEl = document.querySelectorAll("main")  //returns an array (really a node list)
+
+console.log(mainEl)
+
+mainEl[0]
+
+document.mainEl[0].style.backgroundColor = "mainEl(--main-bg)"
